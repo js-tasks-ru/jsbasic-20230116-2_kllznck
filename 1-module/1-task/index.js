4bbefdd38748ebc170f0609cde7ce4af3e69330c
kllzn <d.s.mudragelya@student.khai.edu>
2023-01-19 20:55:30 +0100

    1st module 1st task finished

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  total = number;
+  if (number >= 1) {
+    for(number -= 1; number > 1; number--) {
+      total *= number;
+  }
+  return total;
+  } else if (number === 0) {
+    return 1;
+  }
+  return 'can\'t calculate factorial';
 }
