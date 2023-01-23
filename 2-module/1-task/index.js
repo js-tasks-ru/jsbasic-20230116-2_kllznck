e7c4360405c3c930d192831bee87116bfb89f928
kllzn <d.s.mudragelya@student.khai.edu>
2023-01-23 21:21:16 +0100

    2nd module 3rd task finished

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (let key in salaries) {
+    if (
+      typeof salaries[key] == "number" &&
+      !isNaN(salaries[key]) &&
+      isFinite(salaries[key]) // не могу никак додуматься как убрать все эти проверки
+    ) {
+      result += salaries[key];
+    }
+  }
+  return result;
 }
