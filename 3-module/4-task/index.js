9f7c872b57088820ed7ead3b7e5eae16959e6950
kllzn <d.s.mudragelya@student.khai.edu>
2023-01-27 15:41:47 +0100

    4th task completed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,17 @@
 function showSalary(users, age) {
-  // ваш код...
+  const result = []
+  users.forEach((user) => {
+    if (user.age <= age) {
+      result.push(user.name);
+      result.push(user.balance);
+    }
+  })
+  for (let i = 0; i < result.length - 1; i++) {
+    if(i % 2 != 0) {
+      result[i] = result[i] + '\n'
+    } else {
+      result[i] = result[i] + ', '
+    }
+  }
+  return result.join('') 
 }
