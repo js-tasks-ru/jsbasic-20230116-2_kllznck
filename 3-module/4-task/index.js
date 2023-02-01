80b87a6332f8c3d25426c8915346181488024ebe
kllzn <d.s.mudragelya@student.khai.edu>
2023-02-01 17:49:43 +0100

    4th task reworked

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,17 +1,6 @@
 function showSalary(users, age) {
-  const result = []
-  users.forEach((user) => {
-    if (user.age <= age) {
-      result.push(user.name);
-      result.push(user.balance);
-    }
-  })
-  for (let i = 0; i < result.length - 1; i++) {
-    if(i % 2 != 0) {
-      result[i] = result[i] + '\n'
-    } else {
-      result[i] = result[i] + ', '
-    }
-  }
-  return result.join('') 
+  return users
+    .filter( user => user.age <= age)
+    .map( user => [user.name, ' ' + user.balance])
+    .join("\n");
 }
