d193b06f16ba96eb8ed54fe5c29f2b2b634c353f
kllzn <d.s.mudragelya@student.khai.edu>
2023-02-01 18:52:56 +0100

    1st task completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,7 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(element) {
+  ul = document.createElement("ul");
+  for (let i = 0; i < element.length; i++) {
+    ul.innerHTML += `<li>${element[i].firstName} ${element[i].lastName}</li>`;
+  }
+  return ul;
 }
