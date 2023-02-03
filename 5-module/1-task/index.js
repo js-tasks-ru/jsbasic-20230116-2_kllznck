1120294dfc02471132c04d4724af317b242b48c5
kllzn <d.s.mudragelya@student.khai.edu>
2023-02-03 18:15:59 +0100

    3rd task completed

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,6 +1,6 @@
 function hideSelf() {
   let btn = document.querySelector('.hide-self-button')
-  hideBtn = () => {
+  let hideBtn = () => {
     btn.hidden = true;
   }
   btn.addEventListener('click', hideBtn)
