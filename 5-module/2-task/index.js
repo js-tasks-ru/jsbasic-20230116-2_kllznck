7a0c97a513c797d90daa7c268c151bc9440bb540
kllzn <d.s.mudragelya@student.khai.edu>
2023-02-03 16:09:16 +0100

    2nd task completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button')
+  let text = document.getElementById('text')
+
+  let hide = () => {
+    if(text.hidden) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  }
+
+  btn.addEventListener('click', hide)
 }
