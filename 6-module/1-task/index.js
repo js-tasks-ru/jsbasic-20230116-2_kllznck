ebdce553b9e0dc76888f7633b5c665d6fc584a82
kllzn <d.s.mudragelya@student.khai.edu>
2023-02-12 18:30:56 +0100

    1st task completed

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,61 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  #rows = [];
+
   constructor(rows) {
+    this.#rows = rows;
+    this.#render();
+    this.#onTableClick();
+  }
+
+  #onTableClick = () => {
+    const button = this.elem.querySelectorAll("button");
+    button.forEach((element) => {
+      element.addEventListener("click", () => {
+        element.closest("tr").remove();
+      });
+    });
+  };
+
+  #template() {
+    return `
+    <div>
+      <table>
+        <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Возраст</th>
+                <th>Зарплата</th>
+                <th>Город</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+          ${this.#rows
+            .map((el) => {
+              return `
+            <tr>
+              <td>${el.name}</td>
+              <td>${el.age}</td>
+              <td>${el.salary}</td>
+              <td>${el.city}</td>
+              <td><button class="del">X</button></td>
+            </tr>
+            `;
+            })
+            .join("")}
+        </tbody>
+     </table>
+    </div>
+    
+    `;
+  }
+
+  #render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.#template();
+    this.elem = wrapper.firstElementChild;
   }
 }
